0dd516ca38844c17dab4f010aeea99c2373d0919
CPR160 <119733591@qq.com>
2019-09-26 10:54:56 +0800

    add class 方法

--- teamvjt.py
+++ teamvjt.py
@@ -3,38 +3,35 @@
 import psutil
 import time
 import os
-from tkinter import *
-import threading
+class Process:
+    def prco(self):
+        pids = psutil.pids()
+        for pid in pids:
+            p = psutil.Process(pid)
+            if p.name() == "TeamViewer.exe":
+                name="yes"
+                return name
 
-
-
-
-def prco():
-    pids = psutil.pids()
-    for pid in pids:
-        p = psutil.Process(pid)
-        if p.name() == "TeamViewer.exe":
-            name="yes"
-            return name
-
-def jk():
-    a = 1
-    while a < 99:
-        name=prco()
-        if name=="yes":
-            print("程序运行正常")
-        else:
-            nowtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-            app_dir = r"C:\Program Files (x86)\TeamViewer\TeamViewer.exe"
-            os.popen(app_dir)
-            print(nowtime + ":  程序启动了")
-        time.sleep(60)
-
-jk()
+class Listens(Process):
+    def listening(self):
+        a = 1
+        while a < 99:
+            name = Process.prco(self)
+            if name == "yes":
+                print("程序运行正常")
+            else:
+                nowtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+                app_dir = r"C:\Program Files (x86)\TeamViewer\TeamViewer.exe"
+                os.popen(app_dir)
+                print(nowtime + ":  程序启动了")
+            time.sleep(5)
+start=Listens()
+start.listening()
 
 '''
-top = Tk()
-Button(top, width=8,height=2,text='启动监控',anchor='c',relief='ridge',compound='bottom',command=jk).pack()
-top.mainloop()
+def start():
+    startup = Listens
+    startup.listening()
+if __name__ == '__start__':
+    start()
 '''
-
